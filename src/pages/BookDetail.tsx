df0ca5c771004fe163f078b1e91202f709dd7208
Marek Príbela <m.pribela@gmail.com>
2025-10-31 10:42:25 +0100

    added mui components and cleaned unused code

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -2,6 +2,63 @@ import { useQuery } from "@tanstack/react-query";
 import type { BookDto } from "../lib/types";
 import { api } from "../lib/api";
 import { useParams } from "@tanstack/react-router";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+function getBookDetail(book: BookDto) {
+  return (
+    <Box sx={{ maxWidth: 700 }}>
+      <Card variant="outlined">
+        <React.Fragment>
+          <CardContent>
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14 }}
+            >
+              Book detail
+            </Typography>
+            <Typography variant="h5" component="div">
+              {book.title}
+            </Typography>
+            <Typography variant="body2">{book.author}</Typography>
+          </CardContent>
+          {book.availableCopies > 0 && (
+            <CardActions>
+              <Button size="small">Borrow</Button>
+            </CardActions>
+          )}
+        </React.Fragment>
+      </Card>
+    </Box>
+  );
+}
+
+function getEmptyDetail() {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">
+        <React.Fragment>
+          <CardContent>
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14 }}
+            >
+              Book detail
+            </Typography>
+            <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+              Could not find the book with ISBN
+            </Typography>
+          </CardContent>
+        </React.Fragment>
+      </Card>
+    </Box>
+  );
+}
 
 export default function BookDetail() {
   const { isbn } = useParams({ from: "/$isbn" });
@@ -13,18 +70,5 @@ export default function BookDetail() {
     },
   });
 
-  return isSuccess ? (
-    <section>
-      <h1>{data.title}</h1>
-      <h2>Author: {data.author}</h2>
-      <h2>Available copies: {data.availableCopies}</h2>
-      {data.availableCopies > 0 ? (
-        <p>
-          <button className="border-1 p-1 m-1">Borrow</button>
-        </p>
-      ) : null}
-    </section>
-  ) : (
-    <p>Could not find the book with ISBN {isbn}</p>
-  );
+  return isSuccess ? getBookDetail(data) : getEmptyDetail();
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,32 +2,69 @@ import Header from "../components/Header";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "../lib/api";
 import type { BookListDto } from "../lib/types";
+import { DataGrid, type GridColDef } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
 import { Link } from "@tanstack/react-router";
-import BookRecord from "../components/BookRecord";
+
+type TableBookDefinition = {
+  id: string;
+  author: string;
+  title: string;
+};
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ISBN", width: 200 },
+  { field: "author", headerName: "Author", width: 200 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 1000,
+    renderCell: (param) => (
+      <Link key={param.id} to="/$isbn" params={{ isbn: param.id.toString() }}>
+        {param.value}
+      </Link>
+    ),
+  },
+];
 
 export default function MainPage() {
-  const { data, isSuccess, isError } = useQuery<BookListDto>({
+  const [rows, setRows] = useState<TableBookDefinition[]>([]);
+  const { data, isSuccess } = useQuery<BookListDto>({
     queryKey: ["books"],
     queryFn: async () => {
       const response = await fetch(api.getAllBook());
       return response.json();
     },
   });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setRows(
+        data.books.map((book) => {
+          var bookDefinition: TableBookDefinition = {
+            id: book.ISBN,
+            author: book.author,
+            title: book.title,
+          };
+          return bookDefinition;
+        })
+      );
+    }
+  }, [isSuccess]);
+
   return (
     <div>
       <main>
         <Header />
-        <ul>
-          {isSuccess &&
-            data.books.map((book) => (
-              <Link key={book.ISBN} to="/$isbn" params={{ isbn: book.ISBN }}>
-                <li>
-                  <BookRecord book={book} />
-                </li>
-              </Link>
-            ))}
-          {isError && <li>No book available</li>}
-        </ul>
+        <Paper sx={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSizeOptions={[5, 10]}
+            sx={{ border: 0 }}
+          />
+        </Paper>
       </main>
       <footer></footer>
     </div>
